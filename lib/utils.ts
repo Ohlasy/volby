dce237626e4417867d5dfd408b96a12fad76107a
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-08-31 11:07:10 +0200

    Add basic video styling

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,3 +5,12 @@ export const decodeDictValues =
   <T>(decodeItem: DecoderFunction<T>) =>
   (value: any) =>
     [...dict(decodeItem)(value).values()];
+
+/** Shuffle array in place, returns a reference to the same array */
+export function shuffleInPlace<T>(array: T[]): T[] {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { shuffleInPlace } from "lib/utils";
 import { getPlaylistItems, YTPlaylistItem } from "lib/youtube";
 import { GetStaticProps, NextPage } from "next";
 import LiteYouTubeEmbed from "react-lite-youtube-embed";
@@ -9,11 +10,11 @@ export type PageProps = {
 
 const Page: NextPage<PageProps> = ({ videos }) => {
   return (
-    <div>
-      <section>
+    <div className="bg-slate-100 p-5">
+      <header className="max-w-7xl m-auto">
         <h1>Volební videa</h1>
-      </section>
-      <section>
+      </header>
+      <section className="max-w-7xl m-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
         {videos.map((video) => (
           <Video {...video} />
         ))}
@@ -23,15 +24,17 @@ const Page: NextPage<PageProps> = ({ videos }) => {
 };
 
 const Video = (video: YTPlaylistItem) => (
-  <div style={{ width: "400px" }}>
-    <p>{video.snippet.title}</p>
+  <div key={video.id} className="p-6 bg-white rounded-xl">
     <LiteYouTubeEmbed
       id={video.snippet.resourceId.videoId}
       title={video.snippet.title}
       poster="hqdefault"
       noCookie={true}
     />
-    <p>{video.snippet.description}</p>
+    <h2 className="text-2xl mt-6 text-gray-800 font-semibold mb-2">
+      {video.snippet.title}
+    </h2>
+    <p className="text-gray-600">{video.snippet.description}</p>
   </div>
 );
 
@@ -39,6 +42,7 @@ export const getStaticProps: GetStaticProps<PageProps> = async () => {
   const { YOUTUBE_API_KEY = "" } = process.env;
   const playlistId = "PLPvYKKWRSI7nAl7usr46TbUZ1_3lNyxnI";
   const videos = await getPlaylistItems(YOUTUBE_API_KEY, playlistId);
+  shuffleInPlace(videos);
   return {
     props: { videos },
   };
