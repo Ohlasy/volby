ea3c5e1de9248769616037c687c4a830f0babbd0
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-09-03 18:06:27 +0200

    Add basic styles for new design

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import LiteYouTubeEmbed from "react-lite-youtube-embed";
 import { Layout } from "lib/Layout";
 import { Article, getAllArticles } from "lib/article";
 import { PropsWithChildren } from "react";
+import Image from "next/image";
 import {
   getPlaylistItems,
   getVideoPermalink,
@@ -18,26 +19,54 @@ export type PageProps = {
 const Page: NextPage<PageProps> = (props) => {
   return (
     <Layout>
+      <Header />
       <BetaBanner />
       <VideoSection {...props} />
       <ArticleSection {...props} />
+      <Footer />
     </Layout>
   );
 };
 
 const BetaBanner = () => (
-  <div className="text-center bg-red-500 text-white p-2 w-full">
-    Tohle je testovací verze webu, nešířit, prosím.
+  <div className="bg-lightBrown text-white px-4 py-2 lg:text-center">
+    <Content>⚠️ Testovací verze webu, nešířit, prosím.</Content>
   </div>
 );
 
+const Header = () => (
+  <header className="bg-peach p-4 py-6 pb-10 lg:min-h-[324px] relative">
+    {/* eslint-disable-next-line @next/next/no-img-element */}
+    <img
+      src="/erb.png"
+      width={491}
+      height={297}
+      alt=""
+      className="hidden lg:block absolute bottom-0 right-4"
+    />
+    <Content>
+      <Image src="/heading.svg" alt="Ohlasy volby 22" width={119} height={40} />
+      <h1 className="text-4xl leading-normal font-bold my-3">
+        Komunální a senátní volby <br className="hidden md:block" />
+        v Boskovicích
+      </h1>
+      <p className="text-xl">
+        Všechno, co potřebujete vědět, než půjdete volit
+      </p>
+    </Content>
+  </header>
+);
+
 const VideoSection = ({ videos }: PageProps) => (
-  <div className="bg-slate-100 p-5 pb-20">
-    <Grid>
-      {videos.map((video) => (
-        <Video {...video} key={video.id} />
-      ))}
-    </Grid>
+  <div className="bg-white p-5 md:border-b border-lightGray md:mb-10">
+    <Content>
+      <h2 className="font-bold text-3xl py-6">Rozhovory a články</h2>
+      <Grid>
+        {videos.map((video) => (
+          <Video {...video} key={video.id} />
+        ))}
+      </Grid>
+    </Content>
   </div>
 );
 
@@ -45,72 +74,89 @@ const Video = (video: YTPlaylistItem) => {
   const permalink = getVideoPermalink(video);
   const shortlink = permalink.replace("https://", "");
   return (
-    <Card key={video.id}>
-      <LiteYouTubeEmbed
-        id={video.snippet.resourceId.videoId}
-        title={video.snippet.title}
-        poster="hqdefault"
-        noCookie={true}
-      />
-      <Header>{video.snippet.title}</Header>
-      <p className="text-gray-600">{video.snippet.description}</p>
-      <p className="text-xs mt-4">
-        <a
-          style={{ color: "rgb(148, 82, 0)" }}
-          href={permalink}
-          target="_blank"
-          rel="noreferrer"
-        >
+    <div className="pb-6 mb-3 border-b border-lightGray border-dotted md:border-none">
+      <div className="-mx-5 md:m-0">
+        <LiteYouTubeEmbed
+          id={video.snippet.resourceId.videoId}
+          title={video.snippet.title}
+          poster="hqdefault"
+          noCookie={true}
+        />
+      </div>
+      <h3 className="text-2xl font-bold py-4 pt-5">{video.snippet.title}</h3>
+      <p className="text-lg leading-relaxed">{video.snippet.description}</p>
+      <p className="text-lg mt-4">
+        <a href={permalink} target="_blank" rel="noreferrer">
           {shortlink}
         </a>
       </p>
-    </Card>
+    </div>
   );
 };
 
 const ArticleSection = ({ articles }: PageProps) => (
-  <div className="bg-slate-300 p-5 pb-20 pt-20">
-    <Grid>
-      {articles.map((article) => (
-        <Article {...article} key={article.relativeURL} />
-      ))}
-    </Grid>
+  <div className="bg-white p-5 pt-1">
+    <Content>
+      <Grid>
+        {articles.map((article) => (
+          <Article {...article} key={article.relativeURL} />
+        ))}
+      </Grid>
+    </Content>
   </div>
 );
 
 const Article = (article: Article) => {
   const pubDate = new Date(article.pubDate);
+  const url = `https://ohlasy.info${article.relativeURL}`;
   return (
-    <Card>
-      <div className="aspect-video overflow-hidden">
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src={article.coverPhoto!} alt="" />
-      </div>
-      <Header>{article.title}</Header>
-      <p className="text-gray-400 text-xs">
+    <div className="text-lg border-b border-lightGray border-dotted pb-4 md:border-none last:border-none">
+      <a href={url} className="font-bold leading-relaxed">
+        <h3>{article.title}</h3>
+      </a>
+      <p className="text-gray pt-1 md:hidden">
         <FormattedDate date={pubDate} />
       </p>
-      <p className="text-gray-600 mt-2">{article.perex}</p>
-    </Card>
+      <p className="text-gray pt-1 hidden md:block">
+        <FormattedDate date={pubDate} /> • {article.perex}
+      </p>
+    </div>
   );
 };
+
+const Footer = () => (
+  <footer className="bg-peach min-h-[300px] p-5 py-10 mt-10 text-lg">
+    <Content>
+      <Image src="/heading.svg" alt="Ohlasy volby 22" width={119} height={40} />
+      <ul className="mt-4 bullets">
+        <li>
+          <a href="https://ohlasy.info">ohlasy.info</a>
+        </li>
+        <li>
+          <a href="mailto:ohlasy@ohlasy.info">ohlasy@ohlasy.info</a>
+        </li>
+        <li>
+          <a href="http://youtube.com/channel/UCylOefq0Efb-A452MlTuejw">
+            YouTube
+          </a>
+        </li>
+      </ul>
+    </Content>
+  </footer>
+);
 
 //
 // Common Components
 //
 
-const Header = ({ children }: PropsWithChildren) => (
-  <h2 className="text-2xl mt-6 text-gray-800 font-semibold mb-2">{children}</h2>
-);
-
-const Card = ({ children }: PropsWithChildren) => (
-  <div className="p-6 bg-white rounded-xl drop-shadow">{children}</div>
-);
-
 const Grid = ({ children }: PropsWithChildren) => (
-  <section className="max-w-7xl m-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
+  <section className="max-w-7xl m-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
     {children}
   </section>
+);
+
+const Content = ({ children }: PropsWithChildren) => (
+  <div className="max-w-7xl m-auto">{children}</div>
 );
 
 const FormattedDate = ({ date }: { date: Date }) => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["PT Serif", "serif"],
+    },
+    extend: {
+      colors: {
+        black: "#111111",
+        gray: "#6F6F6F",
+        lightGray: "#DDDDDD",
+        deepBrown: "#422608",
+        lightBrown: "#8B551B",
+        peach: "#FDEAE1",
+      },
+    },
   },
   plugins: [],
 };
